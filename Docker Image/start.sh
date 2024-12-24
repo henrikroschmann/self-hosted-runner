3ad466ea036da43380ec9708c6b8f698b55ca194
Henrik Röschmann <henrik.roschmann@gmail.com>
2024-12-24 12:34:42 +0100

    update start.sh

--- Docker Image/start.sh
+++ Docker Image/start.sh
@@ -4,22 +4,8 @@ REPO=$REPO
 REG_TOKEN=$REG_TOKEN
 NAME=$NAME
 
-cd /home/docker/actions-runner || exit
-./config.sh --url https://github.com/${REPO} --token ${REG_TOKEN} --name ${NAME}
 
-cleanup() {
-  echo "Removing runner..."
-  ./config.sh remove --unattended --token ${REG_TOKEN}
-}
+# Create the runner and start the configuration experience
+./config.sh --url $REPO_URL --token $REPO_TOKEN --name ${NAME}
 
-trap 'cleanup; exit 130' INT
-trap 'cleanup; exit 143' TERM
-
-./run.sh & wait $!
-
-# #!/bin/bash
-
-# # Create the runner and start the configuration experience
-# ./config.sh --url $REPO_URL --token $REPO_TOKEN
-
-# ./run.sh
+./run.sh
