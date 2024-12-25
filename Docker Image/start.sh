4eacf93b13286916ceebafca1695af6122fb3625
Henrik Röschmann <henrik.roschmann@gmail.com>
2024-12-25 12:31:03 +0100

    updated docker file

--- Docker Image/start.sh
+++ Docker Image/start.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-# Expect environment variables to be passed at runtime:
-#   - REPO: GitHub org/repo URL
-#   - REG_TOKEN: GitHub Actions runner registration token
-#   - NAME: Name for this runner
-
 # Configure the runner
 ./config.sh --url "$REPO" --token "$REG_TOKEN" --name "$NAME" --unattended --replace
 
